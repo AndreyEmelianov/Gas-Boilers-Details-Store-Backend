61abfe27bb7902fe881be83051a71fd997bd7a72
Andrey Emelianov <621077@mail.ru>
2023-11-08 19:18:09 +0300

    [feat(users)]: в контроллерах реализовал метод логина пользователя,  проверил через insomnia всё работает корректно, в куках подцепляется sid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,12 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from '../auth/local.auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +21,13 @@ export class UsersController {
   @Header('Content-Type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-Type', 'application/json')
+  login(@Request() req) {
+    return { user: req.user, message: 'Logged in' };
   }
 }
