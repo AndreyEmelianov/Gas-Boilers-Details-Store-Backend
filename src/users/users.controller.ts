476b0c34b4d6c118924089e12d2312e6d0d9e501
Andrey Emelianov <621077@mail.ru>
2023-11-08 19:28:57 +0300

    [feat(users)]: реализовал метод logout, проверил черезе insomnia всё работает, корректно. Авторизация через сессии готова

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,4 +37,10 @@ export class UsersController {
   loginCheck(@Request() req) {
     return req.user;
   }
+
+  @Get('/logout')
+  logout(@Request() req) {
+    req.session.destroy();
+    return { message: 'session has ended' };
+  }
 }
