08d2bc4ce9da90bde8245eb340924c81ac7b0552
Andrey Emelianov <621077@mail.ru>
2023-11-08 19:40:16 +0300

    [feat(users)]: добавил в dto декораторы swagger описывающие схему с примерами полей, проверил в бразуере, всё отрабатывает корректно

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'Vasya' })
   @IsNotEmpty()
   readonly username: string;
 
+  @ApiProperty({ example: 'Vasya@gmail.com' })
   @IsNotEmpty()
   readonly email: string;
 
+  @ApiProperty({ example: 'Vasya1234' })
   @IsNotEmpty()
   readonly password: string;
 }
