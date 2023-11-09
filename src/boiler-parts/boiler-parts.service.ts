14c3e1ed991a21634c5ec58d4eb775a4a0bdf3bd
Andrey Emelianov <621077@mail.ru>
2023-11-09 15:56:49 +0300

    [feat(boiler-parts)]: реализовал методы для поиска bestsellers, для новых продуктов, для поиска одного продукта по id,  для поиска продукта по названию и по совпадению символов в названии

--- src/boiler-parts/boiler-parts.service.ts
+++ src/boiler-parts/boiler-parts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { BoilerParts } from './boiler-parts.model';
 import { IBoilerPartsQuery } from './types';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class BoilerPartsService {
@@ -18,6 +19,39 @@ export class BoilerPartsService {
     return this.boilerPartsModel.findAndCountAll({
       limit,
       offset,
+    });
+  }
+
+  async bestsellers(): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { bestsellers: true },
+    });
+  }
+
+  async newProducts(): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { new_product: true },
+    });
+  }
+
+  async findOne(id: number): Promise<BoilerParts> {
+    return this.boilerPartsModel.findOne({
+      where: { id },
+    });
+  }
+
+  async findOneByName(product_name: string): Promise<BoilerParts> {
+    return this.boilerPartsModel.findOne({
+      where: { product_name },
+    });
+  }
+
+  async searchByString(
+    str: string,
+  ): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      limit: 20,
+      where: { product_name: { [Op.like]: `%${str}%` } },
     });
   }
 }
