d0e86abad6380876cf955edfbb4d0168ed2f288a
Andrey Emelianov <621077@mail.ru>
2023-11-09 17:19:37 +0300

    [feat(boiler-parts)]: описал типы и примеры для документации сваггера и использовах их в декораторах в контроллерах для эндпоинтов, проверил на странице сваггера, документация отображается корректно и примеры корректны

--- src/boiler-parts/boiler-parts.controller.ts
+++ src/boiler-parts/boiler-parts.controller.ts
@@ -9,41 +9,60 @@ import {
 } from '@nestjs/common';
 import { BoilerPartsService } from './boiler-parts.service';
 import { AuthenticatedGuard } from '../auth/authenticated.guard';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import {
+  FindOneResponse,
+  GetBestsellersResponse,
+  GetNewProductsResponse,
+  GetProductByNameRequest,
+  GetProductByNameResponse,
+  PaginateAndFilterResponse,
+  SearchRequest,
+  SearchResponse,
+} from './types';
 
 @Controller('boiler-parts')
 export class BoilerPartsController {
   constructor(private readonly boilerPartsService: BoilerPartsService) {}
 
+  @ApiOkResponse({ type: PaginateAndFilterResponse })
   @UseGuards(AuthenticatedGuard)
   @Get()
   paginateAndFilter(@Query() query) {
     return this.boilerPartsService.paginateAndFilter(query);
   }
 
+  @ApiOkResponse({ type: FindOneResponse })
   @UseGuards(AuthenticatedGuard)
   @Get('find/:id')
   getProductById(@Param('id') id: string) {
     return this.boilerPartsService.findOne(id);
   }
 
+  @ApiOkResponse({ type: GetBestsellersResponse })
   @UseGuards(AuthenticatedGuard)
   @Get('bestsellers')
   getBestsellers() {
     return this.boilerPartsService.bestsellers();
   }
 
+  @ApiOkResponse({ type: GetNewProductsResponse })
   @UseGuards(AuthenticatedGuard)
   @Get('new-products')
   getNewProducts() {
     return this.boilerPartsService.newProducts();
   }
 
+  @ApiOkResponse({ type: SearchResponse })
+  @ApiBody({ type: SearchRequest })
   @UseGuards(AuthenticatedGuard)
   @Post('search')
   search(@Body() { search }: { search: string }) {
     return this.boilerPartsService.searchByString(search);
   }
 
+  @ApiOkResponse({ type: GetProductByNameResponse })
+  @ApiBody({ type: GetProductByNameRequest })
   @UseGuards(AuthenticatedGuard)
   @Post('product-name')
   getProductByName(@Body() { name }: { name: string }) {
--- src/boiler-parts/types/index.ts
+++ src/boiler-parts/types/index.ts
@@ -1,4 +1,113 @@
+import { faker } from '@faker-js/faker';
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface IBoilerPartsQuery {
   limit: string;
   offset: string;
 }
+
+class BoilerParts {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: faker.lorem.sentence(2) })
+  boiler_manufacturer: string;
+
+  @ApiProperty({ example: faker.lorem.sentence(2) })
+  parts_manufacturer: string;
+
+  @ApiProperty({ example: 8370 })
+  product_price: string;
+
+  @ApiProperty({ example: faker.internet.password() })
+  vendor_code: string;
+
+  @ApiProperty({ example: faker.lorem.word() })
+  product_name: string;
+
+  @ApiProperty({ example: faker.lorem.sentence() })
+  product_description: string;
+
+  @ApiProperty({ example: faker.image.technics() })
+  images: string;
+
+  @ApiProperty({ example: 3 })
+  in_stock: number;
+
+  @ApiProperty({ example: false })
+  bestseller: boolean;
+
+  @ApiProperty({ example: false })
+  new_product: boolean;
+
+  @ApiProperty({ example: 45 })
+  popularity_product: number;
+
+  @ApiProperty({ example: faker.lorem.sentence() })
+  compatibility: string;
+
+  @ApiProperty({ example: '2023-01-31T19:46:45.000Z' })
+  createdAt: string;
+
+  @ApiProperty({ example: '2023-01-31T19:46:45.000Z' })
+  updatedAt: string;
+}
+
+export class PaginateAndFilterResponse {
+  @ApiProperty({ example: 10 })
+  count: number;
+
+  @ApiProperty({ type: BoilerParts, isArray: true })
+  rows: BoilerParts;
+}
+
+export class Bestsellers extends BoilerParts {
+  @ApiProperty({ example: true })
+  bestseller: boolean;
+}
+
+export class GetBestsellersResponse extends PaginateAndFilterResponse {
+  @ApiProperty({ example: 10 })
+  count: number;
+
+  @ApiProperty({ type: BoilerParts, isArray: true })
+  rows: Bestsellers;
+}
+
+export class NewProducts extends BoilerParts {
+  @ApiProperty({ example: true })
+  new_product: boolean;
+}
+
+export class GetNewProductsResponse extends PaginateAndFilterResponse {
+  @ApiProperty({ example: 10 })
+  count: number;
+
+  @ApiProperty({ type: BoilerParts, isArray: true })
+  rows: NewProducts;
+}
+
+export class SearchRequest {
+  @ApiProperty({ example: 'om' })
+  search: string;
+}
+
+export class SearchByLetterResponse extends BoilerParts {
+  @ApiProperty({ example: 'Explicabo omnis.' })
+  product_name: string;
+}
+export class SearchResponse extends PaginateAndFilterResponse {
+  @ApiProperty({ type: SearchByLetterResponse, isArray: true })
+  rows: SearchByLetterResponse;
+}
+
+export class GetProductByNameRequest {
+  @ApiProperty({ example: 'Explicabo omnis.' })
+  name: string;
+}
+export class GetProductByNameResponse extends BoilerParts {
+  @ApiProperty({ example: 'Explicabo omnis.' })
+  product_name: string;
+}
+
+export class FindOneResponse extends BoilerParts {}
