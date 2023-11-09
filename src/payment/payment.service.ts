cd7b0d8001aa3db4aaad10ff67729dfec6436517
Andrey Emelianov <621077@mail.ru>
2023-11-09 19:56:50 +0300

    [feat(payment)]: описал dto платежа, подключил Ю-касса, реализовал метод makePayment с помощью axios

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { MakePaymentDto } from './dto/make-payment.dto';
+import axios from 'axios';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  async makePayment(dto: MakePaymentDto) {
+    try {
+      const { data } = await axios({
+        method: 'POST',
+        url: 'https://api.yookassa.ru/v3/payments',
+        headers: {
+          'Content-Type': 'application/json',
+          'Idempotence-Key': Date.now(),
+        },
+        auth: {
+          username: '278408',
+          password: process.env.UKASSA_KEY,
+        },
+        data: {
+          amount: {
+            value: dto.amount,
+            currency: 'RUB',
+          },
+          capture: true,
+          confirmation: {
+            type: 'redirect',
+            return_url: 'http://localhost:3001/order',
+          },
+          description: 'Заказ №1',
+        },
+      });
+
+      return data;
+    } catch (err) {
+      throw new ForbiddenException(err);
+    }
+  }
+}
