f46749b0e4ee20916eb1feb65524bbbe264951b6
Andrey Emelianov <621077@mail.ru>
2023-11-09 17:38:23 +0300

    [feat(shopping-cart)]: реализовал метод получения всех товаров корзины для конкретного пользователя

--- src/shopping-cart/shopping-cart.service.ts
+++ src/shopping-cart/shopping-cart.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { ShoppingCart } from './shopping-cart.model';
+import { UsersService } from '../users/users.service';
+import { BoilerPartsService } from '../boiler-parts/boiler-parts.service';
 
 @Injectable()
-export class ShoppingCartService {}
+export class ShoppingCartService {
+  constructor(
+    @InjectModel(ShoppingCart)
+    private shoppingCartModel: typeof ShoppingCart,
+    private readonly usersService: UsersService,
+    private readonly boilerPartsModel: BoilerPartsService,
+  ) {}
+
+  async findAll(userId: number | string): Promise<ShoppingCart[]> {
+    return this.shoppingCartModel.findAll({
+      where: { userId },
+    });
+  }
+}
