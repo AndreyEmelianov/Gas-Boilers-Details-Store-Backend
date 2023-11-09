867742a64683ba09dba48715bd84b1b92cab58b9
Andrey Emelianov <621077@mail.ru>
2023-11-09 18:30:57 +0300

    [feat(shopping-cart)]: реализовал два эндпоинта в контроллерах для получения всех товаров корзины и для добавления товара в корзину

--- src/shopping-cart/shopping-cart.controller.ts
+++ src/shopping-cart/shopping-cart.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ShoppingCartService } from './shopping-cart.service';
+import { AuthenticatedGuard } from '../auth/authenticated.guard';
+import { AddToCartDto } from './dto/add-to-cart.dto';
 
 @Controller('shopping-cart')
-export class ShoppingCartController {}
+export class ShoppingCartController {
+  constructor(private readonly shoppingCartService: ShoppingCartService) {}
+
+  @UseGuards(AuthenticatedGuard)
+  @Get(':id')
+  getAll(@Param('id') userId: string) {
+    return this.shoppingCartService.findAll(userId);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Post('/add')
+  addToCart(@Body() dto: AddToCartDto) {
+    return this.shoppingCartService.addToCart(dto);
+  }
+}
