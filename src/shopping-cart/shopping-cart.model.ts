f3af0982474e0684dd51b8a7b4a68e4c10fbcf27
Andrey Emelianov <621077@mail.ru>
2023-11-09 17:32:40 +0300

    [feat(shopping-cart)]: описал модель shopping-cart и добавил импорты в module

---
+++ src/shopping-cart/shopping-cart.model.ts
@@ -0,0 +1,34 @@
+import { Model, Table, Column } from 'sequelize-typescript';
+
+@Table
+export class ShoppingCart extends Model {
+  @Column
+  userId: string;
+
+  @Column({ defaultValue: 0 })
+  productId: number;
+
+  @Column
+  boiler_manufacturer: string;
+
+  @Column
+  parts_manufacturer: string;
+
+  @Column({ defaultValue: 0 })
+  product_price: number;
+
+  @Column
+  product_name: string;
+
+  @Column
+  image: string;
+
+  @Column({ defaultValue: 0 })
+  in_stock: number;
+
+  @Column({ defaultValue: 0 })
+  total_price: number;
+
+  @Column({ defaultValue: 0 })
+  count: number;
+}
