a421c072b06bf233bb6e960bc0c7a322dd265c2c
Andrey Emelianov <621077@mail.ru>
2023-11-09 15:38:16 +0300

    [feat(seeders)]: с помощью библиотеки faker заполнил поля таблицы BoilerParts и сделал миграцию в bd, всё отработало корректно, таблица заполнилась рандомными сущностями во всех полях корректно

--- seeders/20231109095222-boiler-parts.js
+++ seeders/20231109095222-boiler-parts.js
@@ -1,25 +1,67 @@
-'use strict';
+const { faker } = require('@faker-js/faker');
+('use strict');
+
+const boilerManufacturers = [
+  'Ariston',
+  'Chaffoteaux&Maury',
+  'Baxi',
+  'Bongioanni',
+  'Saunier Duval',
+  'Buderus',
+  'Strategist',
+  'Henry',
+  'Northwest',
+];
+
+const partsManufacturers = [
+  'Azure',
+  'Gloves',
+  'Cambridgeshire',
+  'Salmon',
+  'Montana',
+  'Sensor',
+  'Lesly',
+  'Radian',
+  'Gasoline',
+  'Croatia',
+];
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(
+      'BoilerParts',
+      [...Array(100)].map(() => ({
+        boiler_manufacturer:
+          boilerManufacturers[
+            Math.floor(Math.random() * boilerManufacturers.length)
+          ],
+        parts_manufacturer:
+          partsManufacturers[
+            Math.floor(Math.random() * partsManufacturers.length)
+          ],
+        vendor_code: faker.internet.password(),
+        product_price: faker.random.numeric(4),
+        product_name: faker.lorem.sentence(2),
+        product_description: faker.lorem.sentence(10),
+        images: JSON.stringify(
+          [...Array(7)].map(
+            () =>
+              `${faker.image.technics()}?random=${faker.random.numeric(30)}`,
+          ),
+        ),
+        in_stock: faker.random.numeric(1),
+        bestseller: faker.datatype.boolean(),
+        new_product: faker.datatype.boolean(),
+        popularity_product: faker.random.numeric(3),
+        compatibility: faker.lorem.sentence(7),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('BoilerParts', null, {});
+  },
 };
