d5a58463e44d060a3651ea03f303cb6af70eade4
Andrey Emelianov <621077@mail.ru>
2023-11-22 17:34:44 +0300

    [feat(boiler-parts)]: расширил полученные квери параметры в методе paginateAndFilter, получение товаров в диапазоне цены и по выбранным фильтрам

--- src/boiler-parts/types/index.ts
+++ src/boiler-parts/types/index.ts
@@ -1,9 +1,20 @@
 import { faker } from '@faker-js/faker';
 import { ApiProperty } from '@nestjs/swagger';
+import { Op } from 'sequelize';
 
 export interface IBoilerPartsQuery {
   limit: string;
   offset: string;
+  boiler: string | undefined;
+  parts: string | undefined;
+  priceFrom: string | undefined;
+  priceTo: string | undefined;
+}
+
+export interface IBoilerPartsFilter {
+  boiler_manufacturer: string | undefined;
+  parts_manufacturer: string | undefined;
+  price: { [Op.between]: number[] };
 }
 
 class BoilerParts {
